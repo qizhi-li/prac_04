6a2b768b4b9b23109a49599e79383e34346b4226
qizhli <qizhi.li@my.jcu.edu.au>
2022-03-31 11:54:48 +0800

    finish

--- prac_03/scores.py
+++ prac_03/scores.py
@@ -1,15 +1,19 @@
 """
 broken_score
 """
+def main():
+    score = float(input("Enter score: "))
+    print(determine_status(score))
+
+def determine_status(score):
+ if score < 0 or score > 100:
+    return "Invalid score"
+ elif score >= 90:
+    return "Excellent"
+ elif score >= 50:
+    return "Passable"
+ else:
+    return "Bad"
 
 
-
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+main()
