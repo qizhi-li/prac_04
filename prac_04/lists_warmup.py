33f1806866984cd33c2c7de1a04a9a625e1e5874
qizhli <qizhi.li@my.jcu.edu.au>
2022-04-11 15:50:59 +0800

    finish

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,19 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+numbers[0] # 0th in the list of numbers
+numbers[-1]#-1th in the list of numbers
+numbers[3]#3th in the list of numbers
+numbers[:-1]#Read a value from 0 to -1
+numbers[3:4]#Read a value from 3 to 4
+5 in numbers#Check if 5 is an element of numbers
+7 in numbers#Check if 7 is an element of numbers
+"3" in numbers#add string 3
+numbers + [6, 5, 3]#add list
+
+# Change the first element of numbers to "ten"
+numbers[0] = "ten"
+# Change the last element of numbers to 1
+numbers[-1] = 1
+# Get all the elements from numbers except the first two
+numbers[2:]
+# Check if 9 is an element of numbers
+9 in numbers
